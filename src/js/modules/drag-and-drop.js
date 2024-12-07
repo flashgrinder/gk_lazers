5363f7137b3e7d8816916d7acbf0932f634ab0b9
flashgrinder <flashgrinder92@gmail.com>
2024-12-07 12:06:44 +0400

    del dropzone

--- src/js/modules/drag-and-drop.js
+++ src/js/modules/drag-and-drop.js
@@ -1,6 +1,3 @@
-// import 'dropzone/dist/basic.css';
-// import 'dropzone/dist/dropzone.css';
-// import Dropzone from "dropzone";
 import * as FilePond from 'filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
@@ -57,58 +54,6 @@ function dragAndDrop() {
             console.log(this);
         });
     });
-
-
-    // Dropzone.autoDiscover = false;
-    // let removeSvg = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-    //                     '<path d="M18 6L6 18" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-    //                     '<path d="M6 6L18 18" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-    //                 '</svg>';
-    //
-    // const allDropzones = document.querySelectorAll('.js-dropzone-form');
-    //
-    // const buildDropzone = (dropzoneElem, key) => {
-    //     return new Dropzone(dropzoneElem, {
-    //         url: window.location.pathname + /upload/,
-    //         clickable: `.js-dropzone-${key}`,
-    //         addRemoveLinks: true,
-    //         autoProcessQueue: false,
-    //         previewsContainer: `.js-dropzone-previews-${key}`,
-    //         hiddenInputContainer: `.js-form-${key}`,
-    //         acceptedFiles: ".jpeg,.jpg,.png,.pdf",
-    //         uploadMultiple: true,
-    //         // hiddenFileInput: "FILES",
-    //         paramName: 'files',
-    //         init: function() {
-    //                 this.hiddenFileInput.setAttribute("name", "FILES[]");
-    //                 // console.log(this.hiddenFileInput);
-    //
-    //                 this.on("addedfiles", function(file) {
-    //                     this.hiddenFileInput.setAttribute("name", "FILES[]");
-    //                     // console.log(this.hiddenFileInput);
-    //                 });
-    //                 let setupHiddenFileInput = ()=> {
-    //                     this.hiddenFileInput.setAttribute("name", "FILES[]");
-    //                 }
-    //
-    //         },
-    //         accept: function(file, done) {
-    //             //произвольная функция проверки загружаемых файлов
-    //             if (file.type == "image/png") {
-    //                 //сообщение без ошибки, если файл забракован
-    //                 console.log("I don`t accept PNGs.");
-    //             }
-    //             //чтобы файл был принят, нужно вызвать done без параметров
-    //             else { done(); }
-    //         },
-    //         previewTemplate: document.querySelector(`.js-form-dropzone-template-${key}`).innerHTML,
-    //         dictRemoveFile: removeSvg,
-    //     });
-    // }
-    //
-    // allDropzones.forEach((dropzone, key) => {
-    //     buildDropzone(dropzone, key);
-    // });
 
 }
 
