641db6c5d5d63a129906e34ce16a997d05717df9
flashgrinder <flashgrinder92@gmail.com>
2025-01-24 10:13:48 +0400

    change product.html tmpl for slider photo

--- src/js/modules/product-content-slider.js
+++ src/js/modules/product-content-slider.js
@@ -1,8 +1,6 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 
-const mediaQuery = window.matchMedia('(max-width: 768px)');
-
 function productContentSlider () {
 
     const productContentSliderTrue = document.querySelector('.js-content-slider-init');
@@ -10,23 +8,37 @@ function productContentSlider () {
         const productContentSlider = new Splide( '.js-content-slider-init', {
             type   : 'carousel',
             rewind: true,
-            height: '343px',
-            perPage: 1,
+            height: '296px',
+            perPage: 4,
             perMove: 1,
             gap: '16px',
             autoplay: true,
-            arrows: false,
+            arrows: true,
             classes: {
                 pagination: 'splide__pagination slider-pagination-dotted',
                 page      : 'splide__pagination__page slider-pagination-dotted__page',
             },
+            breakpoints: {
+                768: {
+                    perPage: 1,
+                    gap: '8px',
+                    arrows: false,
+                },
+            }
         } );
 
-        if(mediaQuery.matches) {
-            productContentSlider.mount();
-        } else {
-            productContentSlider.destroy();
-        }
+        productContentSlider.on( 'mounted', function () {
+            let items = productContentSlider.length;
+            let perPage = productContentSlider.options.perPage;
+            if ( items <= perPage ) {
+                productContentSlider.options = {
+                    drag: false,
+                    arrows: false, // If you're not using the CSS method above you can use this to hide arrows
+                };
+            }
+        });
+
+        productContentSlider.mount();
     }
 
 }
